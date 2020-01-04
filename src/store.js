244645b4e04173c224b08540ea47dd2308ea6276
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2020-01-04 23:51:12 +0100

    env variable for redux devtools

--- src/store.js
+++ src/store.js
@@ -8,21 +8,22 @@ const middleware = [thunk];
 
 let store;
 
-if (process.env.NODE_ENV === 'production') {
-    store = createStore(
-        rootReducer,
-        initialState,
-        compose(
-            applyMiddleware(...middleware)
-        )
-    );
-} else {
+if (process.env.NODE_ENV === 'development') {
     store = createStore(
         rootReducer,
         initialState,
         compose(
             applyMiddleware(...middleware),
             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    );
+
+} else {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware)
         )
     );
 }
