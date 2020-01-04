b977fb900cd55ed7c5f1b502ba4fe97737f4848c
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2020-01-04 23:53:58 +0100

    commented out devtools

--- src/store.js
+++ src/store.js
@@ -6,26 +6,13 @@ const initialState = {};
 
 const middleware = [thunk];
 
-let store;
-
-if (process.env.NODE_ENV === 'development') {
-    store = createStore(
-        rootReducer,
-        initialState,
-        compose(
-            applyMiddleware(...middleware),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-    );
-
-} else {
-    store = createStore(
-        rootReducer,
-        initialState,
-        compose(
-            applyMiddleware(...middleware)
-        )
-    );
-}
+const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware)/*,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
+    )
+);
 
 export default store;
